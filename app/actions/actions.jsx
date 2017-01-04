666fa2ba79b9437990e3adcf9bd8e9f0a56bbec1
Kimmo Halonen <Kimmo Halonen>
2017-01-04 21:38:26 +0200

    Fetch todos from firebase on app start

--- app/actions/actions.jsx
+++ app/actions/actions.jsx
@@ -46,6 +46,25 @@ export var addTodos = (todos) => {
   };
 };
 
+export var startAddTodos = () => {
+  return (dispatch, getState) => {
+      var todosRef = firebaseRef.child('todos');
+
+      return todosRef.once('value').then((snapshot) => {
+        var todos = snapshot.val() || {};
+        var parsedTodos = [];
+
+        Object.keys(todos).forEach((todoId) => {
+          parsedTodos.push({
+            id: todoId,
+            ...todos[todoId]
+          });
+        });
+        dispatch(addTodos(parsedTodos));
+    });
+  };
+};
+
 export var updateTodo = (id, updates) => {
   return {
     type: 'UPDATE_TODO',
@@ -61,7 +80,7 @@ export var startToggleTodo = (id, completed) => {
       completed,
       completedAt: completed ? moment().unix() : null
     };
-    todoRef.update(updates).then(()=> {
+    return todoRef.update(updates).then(()=> {
       dispatch(updateTodo(id, updates));
     });
   };
--- playground/firebase/index.js
+++ playground/firebase/index.js
@@ -35,69 +35,69 @@ var newTodoRef = todosRef.push({
   text: 'Kill the cat!'
 });
 
-// var notesRef = firebaseRef.child('notes');
-//
-// notesRef.on('child_added', (snapshot) => {
-//   console.log('child added', snapshot.key, snapshot.val());
-// });
-//
-// notesRef.on('child_changed', (snapshot) => {
-//   console.log('child added', snapshot.key, snapshot.val());
-// });
-//
-// notesRef.on('child_removed', (snapshot) => {
-//   console.log('child removed', snapshot.key, snapshot.val());
-// });
-//
-// var newNoteRef = notesRef.push({
-//   text: 'Walk the dog!'
-// });
-// console.log('Todo id', newNoteRef.key);
+var notesRef = firebaseRef.child('notes');
 
-// var logData = (snapshot) => {
-//   console.log('Got value', snapshot.val());
-// };
-//
-// firebaseRef.child('user').on('value', logData);
-//
-// firebaseRef.child('user').update({
-//   name: 'Terry'
-// });
-//
-// firebaseRef.child('app').update({
-//   name: 'Todo Application'
-// });
+notesRef.on('child_added', (snapshot) => {
+  console.log('child added', snapshot.key, snapshot.val());
+});
 
-// var logData = (snapshot) => {
-//   console.log('Got value', snapshot.val());
-// };
-//
-// firebaseRef.on('value', logData);
-//
-// firebaseRef.off(logData);
-// firebaseRef.update({isRunning: false});
+notesRef.on('child_changed', (snapshot) => {
+  console.log('child added', snapshot.key, snapshot.val());
+});
 
-// firebaseRef.child('app').once('value').then((snapshot) => {
-//   console.log('Got entire database', snapshot.key, snapshot.val());
-// }, (e) => {
-//   console.log('Unable to fetch value', e);
-// });
+notesRef.on('child_removed', (snapshot) => {
+  console.log('child removed', snapshot.key, snapshot.val());
+});
 
-// firebaseRef.update({
-//   'app/name': 'Todo Application',
-//   'user/name': 'Mike'
-// });
+var newNoteRef = notesRef.push({
+  text: 'Walk the dog!'
+});
+console.log('Todo id', newNoteRef.key);
 
-// firebaseRef.child('app').update({
-//   name: 'Todo Application'
-// });
-//
-// firebaseRef.child('user').update({
-//   name: 'Terry'
-// });
-//
-// firebaseRef.update({
-//   isRunning: null
-// });
-//
-// firebaseRef.child('user/age').remove();
+var logData = (snapshot) => {
+  console.log('Got value', snapshot.val());
+};
+
+firebaseRef.child('user').on('value', logData);
+
+firebaseRef.child('user').update({
+  name: 'Terry'
+});
+
+firebaseRef.child('app').update({
+  name: 'Todo Application'
+});
+
+var logData = (snapshot) => {
+  console.log('Got value', snapshot.val());
+};
+
+firebaseRef.on('value', logData);
+
+firebaseRef.off(logData);
+firebaseRef.update({isRunning: false});
+
+firebaseRef.child('app').once('value').then((snapshot) => {
+  console.log('Got entire database', snapshot.key, snapshot.val());
+}, (e) => {
+  console.log('Unable to fetch value', e);
+});
+
+firebaseRef.update({
+  'app/name': 'Todo Application',
+  'user/name': 'Mike'
+});
+
+firebaseRef.child('app').update({
+  name: 'Todo Application'
+});
+
+firebaseRef.child('user').update({
+  name: 'Terry'
+});
+
+firebaseRef.update({
+  isRunning: null
+});
+
+firebaseRef.child('user/age').remove();
